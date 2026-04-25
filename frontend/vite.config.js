bf5f8568df18749c854a36256b5713381cb2428d
keshrirohan <rohankeshri06@gmail.com>
2026-04-25 14:22:17 +0530

    refactor: update Vite configuration to improve server and preview settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  server: {
+    host: true, // local network access
+    port: 5173
+  },
+
   preview: {
     host: true,
     port: 10000,
-    allowedHosts: "all"
+    allowedHosts: true // allow all hosts (fix Render issue)
+  },
+
+  build: {
+    outDir: "dist",
+    sourcemap: false
   }
 });
