abdf669c3728520eed1b29875007cf4301afcd0f
keshrirohan <rohankeshri06@gmail.com>
2026-04-25 14:30:32 +0530

    refactor: simplify Vite configuration by removing server settings and fixing allowed hosts for preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,20 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
-  server: {
-    host: true, // local network access
-    port: 5173
-  },
-
   preview: {
     host: true,
     port: 10000,
-    allowedHosts: true // allow all hosts (fix Render issue)
-  },
-
-  build: {
-    outDir: "dist",
-    sourcemap: false
+    allowedHosts: [
+      "nprs-system-frontend-1.onrender.com"
+    ]
   }
 });
